e2df148842a3d862e56c3afddfa2f04fc2cd6628
PanAndy <1163962054@qq.com>
2019-11-15 12:56:38 +0800

    add save and load model codes

--- tools.py
+++ tools.py
@@ -32,3 +32,34 @@ def imshow(img):
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
+
+
+class TheModelClass(nn.Module):
+    def __init__(self):
+        super(TheModelClass, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc_the_model_class = FC()
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = self.fc_the_model_class(x)
+        return x
+
+
+class FC(nn.Module):
+    def __init__(self):
+        super(FC, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(16 * 5 * 5, 120),
+            nn.ReLU(),
+            nn.Linear(120, 84),
+            nn.ReLU(),
+            nn.Linear(84, 10)
+        )
+
+    def forward(self, x):
+        return self.fc(x)
